c2a7c6f628ddd1215e8b47a9ef11cebb64aaad4c
sumedh91 <sumedh.youth@gmail.com>
2017-10-25 20:29:28 +0530

    First Commit

---
+++ assets/js/app.js
@@ -0,0 +1,14 @@
+// MODULE
+var angularApp = angular.module('angularApp', []);
+
+
+app.run(function($rootScope) {
+    document.addEventListener("keyup", function(e) {
+        if (e.keyCode === 27)
+            $rootScope.$broadcast("escapePressed", e.target);
+    });
+
+    document.addEventListener("click", function(e) {
+        $rootScope.$broadcast("documentClicked", e.target);
+    });
+});
